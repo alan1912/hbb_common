e3d58991ac54e1ce1af58c2b4fa1a2693529245c
Alan <alan@boxful.com.tw>
2025-10-15 12:16:20 +0800

    adjust

--- src/config.rs
+++ src/config.rs
@@ -67,7 +67,6 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
         map.insert("whitelist".to_owned(), "61.222.27.27".to_owned()); // 白名單
-        map.insert("enable-remote-printer".to_owned(), "N".to_owned()); // 關閉印表機功能
         RwLock::new(map)
     };
 
@@ -94,7 +93,19 @@ lazy_static::lazy_static! {
         map.insert("disable-account".to_owned(), "Y".to_owned()); // 禁止帳號
         RwLock::new(map)
     };
-    pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
+
+    // pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
+    pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = {
+        let mut map = HashMap::new();
+        map.insert("hide-remote-printer-settings".to_owned(), "Y".to_owned()); // 隱藏印表機設定分頁
+        map.insert("enable-hwcodec".to_owned(), "Y".to_owned()); // 固定啟用硬體編解碼器
+        map.insert("enable-abr".to_owned(), "Y".to_owned()); // 固定啟用自適應位元速率
+        map.insert("allow-auto-record-incoming".to_owned(), "N".to_owned()); // 固定關閉自動錄製連入
+        map.insert("allow-auto-record-outgoing".to_owned(), "N".to_owned()); // 固定關閉自動錄製連出
+        map.insert("allow-remove-wallpaper".to_owned(), "N".to_owned()); // 固定關閉移除桌布
+        map.insert("audio-input".to_owned(), "".to_owned()); // 固定音訊輸入裝置為系統預設
+        RwLock::new(map)
+    };
 }
 
 lazy_static::lazy_static! {
